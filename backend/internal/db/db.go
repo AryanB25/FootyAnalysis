45050dad007fb53c616ba00016a8b85d5424752e
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-07-28 14:36:49 -0600

    Database connection

--- backend/internal/db/db.go
+++ backend/internal/db/db.go
@@ -1,1 +1,18 @@
 package db
+
+import (
+	"database/sql"
+)
+
+func Connect() (*sql.DB, error) {
+	connectionString := "host=localhost port=5432 user=postgres password=pass dbname=football sslmode=disable"
+	database, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		return nil, err
+	}
+	err = database.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return database, err
+}
