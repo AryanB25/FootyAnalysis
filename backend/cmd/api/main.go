a2021563a300c7ceac06d9cf67a43fa7d0a0a778
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-07-28 14:36:34 -0600

    Database connection

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,1 +1,24 @@
-package api
+package main
+
+import (
+	"footballanalyticshub/internal/db"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	database, err := db.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
+	router := gin.Default()
+	router.GET("/health", returnMessage)
+	router.Run("localhost:8080")
+}
+
+func returnMessage(c *gin.Context) {
+	c.String(http.StatusOK, "Football Analytics Hub API is up")
+}
