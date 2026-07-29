ec854bb90d8e1ef764cfd1e13833533b6fa0d669
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-07-29 16:32:13 -0600

    Column specification

--- backend/internal/db/migrations.sql
+++ backend/internal/db/migrations.sql
@@ -4,12 +4,10 @@ CREATE TABLE IF NOT EXISTS players (
     short_name TEXT,
     long_name TEXT,
     age INTEGER,
-    dob DATE,
     nationality_name TEXT,
 
     club_name TEXT,
     league_name TEXT,
-    league_level INTEGER,
     club_position TEXT,
 
     height_cm INTEGER,
@@ -29,24 +27,4 @@ CREATE TABLE IF NOT EXISTS players (
     dribbling INTEGER,
     defending INTEGER,
     physic INTEGER,
-
-    attacking_finishing INTEGER,
-    attacking_short_passing INTEGER,
-    skill_dribbling INTEGER,
-    skill_ball_control INTEGER,
-    power_shot_power INTEGER,
-    power_stamina INTEGER,
-    power_strength INTEGER,
-
-    mentality_aggression INTEGER,
-    mentality_positioning INTEGER,
-    mentality_vision INTEGER,
-    mentality_penalties INTEGER,
-    mentality_composure INTEGER,
-
-    goalkeeping_diving INTEGER,
-    goalkeeping_handling INTEGER,
-    goalkeeping_kicking INTEGER,
-    goalkeeping_positioning INTEGER,
-    goalkeeping_reflexes INTEGER
 );
