016232d39fb1755283f738407094ce3be0dd4bb8
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-07-30 13:20:09 -0600

    Handlers and Repositories

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"footballanalyticshub/internal/db"
 	"footballanalyticshub/internal/db/seed"
+	"footballanalyticshub/internal/players"
 	"log"
 	"net/http"
 
@@ -19,8 +20,13 @@ func main() {
 		log.Fatal(err)
 	}
 	defer database.Close()
+	repo := players.NewPlayerRepository(database)
+	handler := players.NewPlayerHandlers(repo)
 	router := gin.Default()
 	router.GET("/health", returnMessage)
+	router.GET("/players/search", handler.SearchPlayers)
+	router.GET("/players/:id", handler.GetPlayerByID)
+	router.GET("/players", handler.GetPlayers)
 	router.Run("localhost:8080")
 }
 
